dbf3c153b5cad2058847cdc69b77f3a3d652592d
Ed Courage <edcourage@live.com>
2019-12-16 15:42:17 +0000

    account tracker now intergrated with accounts

--- lib/account.rb
+++ lib/account.rb
@@ -1,16 +1,20 @@
+require_relative 'account_tracker'
 class Account
   attr_reader :current_balance
 
-  def initialize
+  def initialize(account_tracker = AccountTracker.new)
+    @account_tracker = account_tracker
     @current_balance = 0
   end
 
   def deposit(amount)
     @current_balance += amount
+    @account_tracker.deposits_tracker(amount, @current_balance)
   end
 
   def withdrew(amount)
     @current_balance -= amount
+    @account_tracker.withdrawals_tracker(amount, @current_balance)
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,14 +1,24 @@
 require 'account'
 
 describe Account do
+  let(:account_tracker) { double 'account_tracker' }
+  let(:account) { Account.new(account_tracker) }
 
-  let(:account) { Account.new }
-
+  before(:each) do
+    allow(account_tracker).to receive(:deposits_tracker)
+    allow(account_tracker).to receive(:withdrawals_tracker)
+  end
   context '#deposit' do
     it 'money is added to account' do
       account.deposit(1000)
       expect(account.current_balance).to eq 1000
     end
+
+    it 'deposits are tracked' do
+      expect(account_tracker).to receive(:deposits_tracker).with(1000, 1000)
+      account.deposit(1000)
+    end
+
   end
 
   context '#withdrew' do
@@ -16,6 +26,12 @@ describe Account do
       account.deposit(1000)
       account.withdrew(500)
       expect(account.current_balance).to eq 500
+    end
+
+    it 'withdrawals are tracked' do
+      account.deposit(1000)
+      expect(account_tracker).to receive(:withdrawals_tracker).with(500, 500)
+      account.withdrew(500)
     end
   end
 
