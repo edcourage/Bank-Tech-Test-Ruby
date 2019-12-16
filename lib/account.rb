15a1a314e625dd4e0dd6eb83916f27f43709a7e2
Ed Courage <edcourage@live.com>
2019-12-16 13:47:48 +0000

    user can now deposit money in account

---
+++ lib/account.rb
@@ -0,0 +1,11 @@
+class Account
+  attr_reader :current_balance
+
+  def initialize
+    @current_balance = 0
+  end
+
+  def deposit(amount)
+    @current_balance += amount
+  end
+end
---
+++ spec/account_spec.rb
@@ -0,0 +1,12 @@
+require 'account'
+
+describe Account do
+
+  let (:account) { Account.new }
+
+  context '#deposit' do
+    it 'money is added to account' do
+      expect { account.deposit(1000) }.to change { account.current_balance }.by(1000)
+    end
+  end
+end
