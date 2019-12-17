355c5cdb02d4a8e1b2796b7b0cb5fb0061123a08
Ed Courage <edcourage@live.com>
2019-12-17 16:26:37 +0000

    display class refactor

--- lib/display.rb
+++ lib/display.rb
@@ -1,15 +1,25 @@
 class Display
 
   def self.bank_statement(account_action_log_array)
-    bank_statement_array = ["date || credit || debit || balance"]
+    bank_statement_array = [bank_statement_header]
     account_action_log_array.each do |action|
-      if action[:type] == :deposit
-        bank_statement_array << "#{action[:date]} || #{("%.2f" % action[:amount])} || || #{("%.2f" % action[:balance])}"
-      elsif action[:type] == :withdrawal
-        bank_statement_array << "#{action[:date]} || || #{("%.2f" % action[:amount])} || #{("%.2f" % action[:balance])}"
-      end
+      bank_statement_array << account_action_display_config(action)
     end
     puts bank_statement_array.join("\n")
   end
+
+  def self.account_action_display_config(action)
+    if action[:type] == :deposit
+      "#{action[:date]} || #{("%.2f" % action[:amount])} || || #{("%.2f" % action[:balance])}"
+    elsif action[:type] == :withdrawal
+      "#{action[:date]} || || #{("%.2f" % action[:amount])} || #{("%.2f" % action[:balance])}"
+    end
+  end
+
+  def self.bank_statement_header
+    "date || credit || debit || balance"
+  end
+
+  private_class_method :account_action_display_config, :bank_statement_header
 
 end
